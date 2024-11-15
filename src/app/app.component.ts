287a04f5e9e18c3f06c568193636b634f77d3044
Guillermo <guillegaindo45@gmail.com>
2024-11-15 15:22:53 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LogUpdateService } from './service/log-update.service';
+import { PushNotificationService } from './service/push-notification.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pwa';
+  title = 'pwa ';
+
+  constructor(private logUpdate: LogUpdateService, private pushService: PushNotificationService) {
+
+
+  }
+
+  ngOnInit() {
+
+    this.logUpdate.subscribe();
+    this.pushService.subscribeToNotifications();
+    this.pushService.listenForNotifications();
+  }
 }
